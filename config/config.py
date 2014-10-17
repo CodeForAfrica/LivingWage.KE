5499f13cf49ad64aa5aafaee01ee51b17aa98eaf
Greg Kempe <greg@kempe.net>
2014-10-17 15:10:43 +0200

    Fully static web app hosted on github pages.
    Remove flask and python, this is a fully static web app
    without complex requirements. This means we can host it directly
    from gh-pages.

--- config/config.py
+++
@@ -1,6 +0,0 @@
-"""
-Add any config variables here that don't have to be private.
-"""
-
-DEBUG = False
-SECRET_KEY = "AEuaheiaer AERGJAOEIRJGae ov4t5AE"
--- runserver.py
+++
@@ -1,6 +0,0 @@
-from app.flask_app import app
-
-if __name__ == "__main__":
-    # run Flask dev-server
-    app.debug = True
-    app.run(port=5000)
