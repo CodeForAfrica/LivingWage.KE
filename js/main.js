f8fa197a513460dab3848161a16ffdea7d3dea00
fritzjooste <fritz@fritzjooste.com>
2015-04-01 11:01:42 +0200

    Clicking on an assumption menu icon now scrolls to that assumption

--- js/main.js
+++ js/main.js
@@ -249,6 +249,20 @@ function change_twitter_text(txt) {
 
 $(document).ready(function() {
 
+    // function for scrolling around website (links to "#section" will scroll to that section)
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
     $("#pay-amount").focus();
 
     // initialize dropdown selects
